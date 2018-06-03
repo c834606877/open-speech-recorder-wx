995a13443fe20da26cbe1823b35519dcbec1d4d0
Lan Bing <c834606877@163.com>
2018-06-03 11:07:32 +0800

    update

--- pages/record/record.js
+++ pages/record/record.js
@@ -5,7 +5,7 @@
 const app = getApp()
 
 const recorderManager = wx.getRecorderManager()
-const innerAudioContext = wx.createInnerAudioContext()
+
 const options = {
   duration: 60000,
   sampleRate: 16000,
@@ -17,19 +17,20 @@ const options = {
 var savedFilePath;
 var RecordStatus;
 var wantedWords = [
-  { word: '小鹿小鹿', ename: 'xiaoluxiaolu', count: 3 },
-  { word: '开始', ename: 'start', count: 2 },
-  { word: '结束', ename: 'end', count: 2 },
-  { word: '向前', ename: 'forward',count: 1 },
-  { word: '向后', ename: 'behind', count: 1 },
-  { word: '向左', ename: 'left', count: 1 },
-  { word: '向右', ename: 'right', count: 1 },
-  { word: '确认', ename: 'confirm', count: 1 },
-  { word: '取消', ename: 'cancel', count: 2 },
-  { word: '暂停', ename: 'pause', count: 2 },
+  { word: '小鹿小鹿', ename: 'xiaoluxiaolu', ncount: 3 },
+  { word: '开始', ename: 'start', ncount: 2 },
+  { word: '结束', ename: 'end', ncount: 2 },
+  { word: '向前', ename: 'forward', ncount: 1 },
+  { word: '向后', ename: 'behind', ncount: 1 },
+  { word: '向左', ename: 'left', ncount: 1 },
+  { word: '向右', ename: 'right', ncount: 1 },
+  { word: '确认', ename: 'confirm', ncount: 1 },
+  { word: '取消', ename: 'cancel', ncount: 2 },
+  { word: '暂停', ename: 'pause', ncount: 2 },
 ];
 var iCurrentWord = 0;
-
+var record_timer = null;
+var record_time = 0;
 
 Page({
 
@@ -37,12 +38,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-    btn_touch_text: "长按录制",
     current_word: "",
+    btn_touch_text: "长按录制",
+    record_time_text: "　",
+    btn_record_class: "btn-record-untouched",
+    btn_reset_enable: false,
     btn_start_enable: "",
     btn_stop_enable: "none",
     btn_upload_enable: "",
-    last_words_cnt: "17"
+    last_words_cnt: "17",
+    current_word_tip: ""
   
   },
   
@@ -51,9 +56,13 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.ResetWordsCnt();
+    iCurrentWord=0;
     this.setData({ current_word: wantedWords[iCurrentWord].word });
     this.setData({ last_words_cnt: this.GetWordsCnt()});
+    this.setData({ btn_reset_enable: false});
+    
+
   },
 
   /**
@@ -65,56 +74,96 @@ Page({
 
     recorderManager.onStop((res) => {
       wx.showToast({
-        title: "成功，正在上传。",
+        title: "正在上传",
         icon: "success"
       })
       console.log('recorder stop')
       var tmpFilePath = res.tempFilePath;
-      wx.saveFile({
-        tempFilePath: tmpFilePath,
-        success: function (res) {
-          savedFilePath = res.savedFilePath;
-          console.log(res);
-          wx.uploadFile({
-            url: 'https://osr.emlab.net/upload_mp3',
-            filePath: savedFilePath,
-            name: 'file',
-            header: {},
-            formData: {
-              word: wantedWords[iCurrentWord].ename,
-              session_id: app.globalData.uid
-            },
-            success: function (res) {
-              console.log(res);
-              console.log("Upload ok");
-            },
-            fail: function (res) {
-              console.log("Upload fail");
-            },
-            complete: function (res) {
-              console.log("Upload complete");
-            },
-          })
 
+      wx.uploadFile({
+        url: 'https://osr.emlab.net/upload_mp3',
+        filePath: tmpFilePath,
+        name: 'file',
+        header: {},
+        formData: {
+          word: wantedWords[iCurrentWord].ename,
+          session_id: app.globalData.uid
         },
-        fail: function (res) { },
+        success: function (res) {
+          console.log(res);
+          console.log("Upload ok");
+        },
+        fail: function (res) {
+          console.log("Upload fail");
+        },
         complete: function (res) {
+          console.log("Upload complete");
           if (wantedWords[iCurrentWord].count > 0) {
             wantedWords[iCurrentWord].count--;
           };
           iCurrentWord = _this.GetWord()
           if (iCurrentWord < 0) {
-            _this.setData({ btn_record_text : "录制完成" });
+            _this.setData({ current_word: "录制完成，谢谢！" });
+            _this.setData({ btn_reset_enable: true });
           }
-          else{
+          else {
             _this.setData({ current_word: wantedWords[iCurrentWord].word });
           }
-          
-
-
         },
       })
+
     });
+
+
+
+
+      // wx.saveFile({
+      //   tempFilePath: tmpFilePath,
+      //   success: function (res) {
+      //     savedFilePath = res.savedFilePath;
+      //     console.log(res);
+      //     wx.uploadFile({
+      //       url: 'https://osr.emlab.net/upload_mp3',
+      //       filePath: savedFilePath,
+      //       name: 'file',
+      //       header: {},
+      //       formData: {
+      //         word: wantedWords[iCurrentWord].ename,
+      //         session_id: app.globalData.uid
+      //       },
+      //       success: function (res) {
+      //         console.log(res);
+      //         console.log("Upload ok");
+      //       },
+      //       fail: function (res) {
+      //         console.log("Upload fail");
+      //       },
+      //       complete: function (res) {
+      //         console.log("Upload complete");
+      //       },
+      //     })
+
+      //   },
+      //   fail: function (res) { },
+      //   complete: function (res) {
+      //     if (wantedWords[iCurrentWord].count > 0) {
+      //       wantedWords[iCurrentWord].count--;
+      //     };
+      //     iCurrentWord = _this.GetWord()
+      //     if (iCurrentWord < 0) {
+      //       _this.setData({ current_word : "录制完成" });
+      //       _this.setData({ btn_reset_enable: true });
+      //     }
+      //     else{
+      //       _this.setData({ current_word: wantedWords[iCurrentWord].word });
+      //     }
+
+      //   },
+      // })
+
+
+
+
   },
 
   /**
@@ -180,6 +229,12 @@ Page({
     
     
   },
+  ResetWordsCnt: function(){
+    for (var i = 0; i < wantedWords.length; i++) {
+      wantedWords[i]['count'] = wantedWords[i].ncount
+    }
+  }
+  ,
   GetWordsCnt: function(){
     var cnt=0;
     for (var i = 0; i < wantedWords.length; i++) {
@@ -207,63 +262,113 @@ Page({
     }
     return -1;
   },
-  RecordWord: function()
-  {
-    let i = this.GetWord();
-    if(i == -1 ){
-      console.log("no more word!");
-      wx.showToast({
-        title: "录制结束，数据已上传",
-        icon: "success"
-      })
-      this.setData({ current_word: "录制完成！" });
-      return;
-    }
-    
-    console.log(wantedWords[i]);
-    console.log("start record word: " + wantedWords[i].word);
-    this.setData({ current_word: wantedWords[i].word });
-
-
-
-  },
-  uploadRecord: function(e)
-  {
-    console.log(e);
-    console.log("uploadRecord btn tap");
-    console.log(savedFilePath);
-    this.playRecord(e);
-    
-  },
-  playRecord: function(e)
-  {
-    innerAudioContext.autoplay = true;
-    innerAudioContext.src = savedFilePath;
-
-
-      innerAudioContext.onPlay(() => {
-        console.log('开始播放')
-      })
-    innerAudioContext.onError((res) => {
-      console.log(res.errMsg)
-      console.log(res.errCode)
-    })
-  },
   touchstart: function(e)
   {
     this.setData({ btn_touch_text : "录音中" });
+    this.setData({ current_word_tip: "请说：" });
+    this.setData({ btn_record_class: "btn-record-touched" });
+    
     recorderManager.start(options);
+
+    // wx.startRecord({
+    //   success: function (res) {
+
+    //     wx.showToast({
+    //       title: "正在上传",
+    //       icon: "success"
+    //     })
+    //     console.log('recorder stop')
+    //     var tmpFilePath = res.tempFilePath;
+
+    //     wx.uploadFile({
+    //       url: 'https://osr.emlab.net/upload_mp3',
+    //       filePath: tmpFilePath,
+    //       name: 'file',
+    //       header: {},
+    //       formData: {
+    //         word: wantedWords[iCurrentWord].ename,
+    //         session_id: app.globalData.uid
+    //       },
+    //       success: function (res) {
+    //         console.log(res);
+    //         console.log("Upload ok");
+    //       },
+    //       fail: function (res) {
+    //         console.log("Upload fail");
+    //       },
+    //       complete: function (res) {
+    //         console.log("Upload complete");
+    //         if (wantedWords[iCurrentWord].count > 0) {
+    //           wantedWords[iCurrentWord].count--;
+    //         };
+    //         iCurrentWord = _this.GetWord()
+    //         if (iCurrentWord < 0) {
+    //           _this.setData({ current_word: "录制完成" });
+    //           _this.setData({ btn_reset_enable: true });
+    //         }
+    //         else {
+    //           _this.setData({ current_word: wantedWords[iCurrentWord].word });
+    //         }
+    //       },
+    //     })
+
+
+
+
+    //   },
+    //   fail: function (res) {
+    //     //录音失败 
+    //     wx.showModal({
+    //       title: '提示',
+    //       content: '录音的姿势不对!',
+    //       showCancel: false,
+    //       success: function (res) {
+    //         if (res.confirm) {
+    //           UTIL.log('用户点击确定')
+    //           return
+    //         }
+    //       }
+    //     })
+    //   }
+    // })
+
+
+
     var _this = this;
 
-    setTimeout(function () {
-      _this.touchend(e)
+    // setTimeout(function () {
+    //   _this.touchend(e)
 
-    }, 10000);
+    // }, 10000);
+
+    _this.setData({ record_time_text: "时长:" + record_time });
+    clearInterval(record_timer);
+    record_timer = setInterval(function (){
+      record_time++;
+      _this.setData({ record_time_text: "时长:" + record_time });
+    },1000)
+
+    
   },
   touchend: function(e)
   {
+    clearInterval(record_timer);
     recorderManager.stop();
+    //wx.stopRecord()
     this.setData({ btn_touch_text: "长按录制" });
+    this.setData({ current_word_tip: "" });
+    this.setData({ btn_record_class: "btn-record-untouched" });
+
+    record_time = 0;
+    this.setData({ record_time_text: "　" });
+  },
+  ResetRecord: function(e){
+    this.onLoad()
+  },
+  CencelRecord: function(e){
+    wx.navigateBack({
+      delta: 3
+    })
   }
 
 
